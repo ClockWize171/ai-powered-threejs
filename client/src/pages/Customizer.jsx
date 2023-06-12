d89cfb1b0739c6dbe2067afd0e9b5866ba18208f
unknown <jupiterminhtin998@gmail.com>
2023-06-12 11:37:40 +0630

    added color picker function

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -12,7 +12,30 @@ import { fadeAnimation, slideAnimation } from '../config/motion'
 import { AiPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components'
 
 const Customizer = () => {
-  const snap = useSnapshot(state)
+  const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  })
+
+  // show tab content depending on activeTab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case 'colorpicker':
+        return <ColorPicker />
+      case 'filepicker':
+        return <FilePicker />
+      case 'aipicker':
+        return <AiPicker />
+      default:
+        return null;
+    }
+  }
 
   return (
     <AnimatePresence>
@@ -28,9 +51,10 @@ const Customizer = () => {
                   <Tab
                     key={tab.name}
                     tab={tab}
-                    handleClick={() => { }}
+                    handleClick={() => { setActiveEditorTab(tab.name) }}
                   />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
